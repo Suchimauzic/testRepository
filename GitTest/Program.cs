cffdcd7d10e19070c42989bdc782ab83953c1a9d
Сучимауз <sergdog03@gmail.com>
2023-10-27 09:44:51 +0300

    Координаты точек

---
+++ GitTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, y1, x2, y2;
+            Random rand = new Random();
+
+            x1 = rand.NextDouble() * 100;
+            y1 = rand.NextDouble() * 100;
+            x2 = rand.NextDouble() * 100;
+            y2 = rand.NextDouble() * 100;
+
+            Console.WriteLine($"Точки: (x1, y1) = ({x1}, {y1}), (x2, y2) = ({x2}, {y2})");
+
+            Console.ReadKey();
+        }
+    }
+}
